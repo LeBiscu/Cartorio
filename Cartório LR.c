b7e18fa5eeca2913abc46671d15339a58a2785c5
LeBiscu <157022571+LeBiscu@users.noreply.github.com>
2024-05-28 10:04:05 -0300

    opção de saída do sistema
    incluso no sistema a opção de saída

--- Cartório LR.c
+++ Cartório LR.c
@@ -125,6 +125,7 @@ int x=1;
 	printf("\t1-Registrar nomes\n");
     printf("\t2-Consultar nomes\n");
 	printf("\t3-Deletar nomes\n");
+	printf("\t4-Sair do sistema\n");
 	
 	scanf("%d",&opcao);
 	
@@ -148,6 +149,11 @@ int x=1;
 	system("pause");
 	break;
 	
+	case 4:
+	printf("Obrigado por utilizar o sistema!\n");
+	return 0;
+	break;
+		
 	default	:
 	printf("Essa opção não existe\n");
     system("pause");
